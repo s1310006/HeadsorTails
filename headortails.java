abccc78dacbb02c095e4bea8b3b1bae76ec529f6
s1310006 <s1310006@u-aizu.ac.jp>
2024-07-02 11:50:30 +0900

    Update headortails.java
    User name added

--- headortails.java
+++ headortails.java
@@ -1,7 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class HeadorTails {
     public static void main(String[] args){
+	    
+	Scanner s = new Scanner(System.in);
+	String name = new String();
+	System.out.println("Who are you?");
+	System.out.print(">");
+	name=s.nextLine();
+	System.out.println("Hallo,"+name+"!");
+	    
 	Random rand = new Random();
 	int head=0,tail=0;
 	System.out.println("Tossing a coin...");
